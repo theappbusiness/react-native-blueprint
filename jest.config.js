44153a4252a10bc36139eaef182bec68a136dc4f
Talha <muhammadtalhakhan1@gmail.com>
2022-10-23 13:39:14 +0100

    fix: remove obselete snapshots and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    preset: 'react-native',
-    transformIgnorePatterns: [
-      'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
-    ],
-  };
+  preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
+  ],
+  modulePathIgnorePatterns: ['src/__tests__/e2e/'],
+};
